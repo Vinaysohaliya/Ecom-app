4dc0eb8db67cb8362626751036eb363d2ee86f6e
VinaySohaliya <Sohaliyavin01@gmail.com>
2023-09-13 23:03:11 +0530

    cardpage update

--- src/pages/cartItem.js
+++ src/pages/cartItem.js
@@ -12,11 +12,19 @@ function Cart() {
 
   return (
     <div>
-      <h2>Your Shopping Cart</h2>
+      <h2 className="text-3xl font-bold mb-4">Your Shopping Cart</h2>
       {cart.map((product) => (
-        <div key={product.id}>
-          <p>{product.name}</p>
-          <button onClick={() => handleRemoveFromCart(product)}>Remove</button>
+        <div key={product.id} className="bg-white p-4 rounded-lg shadow-md">
+          <img src={product.imageUrl} alt={product.name} className="w-full h-40 object-cover rounded" />
+          <h2 className="text-xl font-semibold mt-2">{product.name}</h2>
+          <p className="text-gray-600">{product.description}</p>
+          <p className="text-lg font-semibold mt-2">Price: ${product.price}</p>
+          <button
+            onClick={() => handleRemoveFromCart(product)}
+            className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 mt-2 rounded"
+          >
+            Remove
+          </button>
         </div>
       ))}
     </div>
--- src/pages/product/[productId].js
+++ src/pages/product/[productId].js
@@ -1,48 +1,55 @@
+// [productid].js (inside pages directory)
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../../Redux/cart/cartSlice';
 import products from '../product.json';
 import { useRouter } from 'next/router';
+import Link from 'next/link'; // Import the Link component
 
 function ProductDetailPage() {
-  const dispatch = useDispatch();
-  const router = useRouter();
+    const dispatch = useDispatch();
+    const router = useRouter();
 
-  const handleAddToCart = (product) => {
-    dispatch(addToCart(product));
-  };
+    const handleAddToCart = (product) => {
+        dispatch(addToCart(product));
+    };
 
-  const { productId } = router.query;
+    const { productId } = router.query;
 
-  const selectedProduct = products.find((product) => product.id === parseInt(productId));
+    const selectedProduct = products.find((product) => product.id === parseInt(productId));
 
-  if (selectedProduct) {
+    if (selectedProduct) {
+        return (
+            <div>
+                <h1 className="text-3xl font-bold mb-4">Product Details</h1>
+                <div className="bg-white p-4 rounded-lg shadow-md">
+                    <img src={selectedProduct.imageUrl} alt={selectedProduct.name} className="w-full h-40 object-cover rounded" />
+                    <h2 className="text-xl font-semibold mt-2">{selectedProduct.name}</h2>
+                    <p className="text-gray-600">{selectedProduct.description}</p>
+                    <p className="text-lg font-semibold mt-2">Price: ${selectedProduct.price}</p>
+                    <button
+                        onClick={() => handleAddToCart(selectedProduct)}
+                        className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 mt-2 rounded"
+                    >
+                        Add to Cart
+                    </button>
+
+                    {/* Link to the cart page */}
+                    <Link href="/cartItem" className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 mt-2 rounded">
+                        Go to Cart
+                    </Link>
+                </div>
+            </div>
+        );
+    }
+
+    // If the product doesn't exist (invalid ID), display a message or redirect to an error page
     return (
-      <div>
-        <h1 className="text-3xl font-bold mb-4">Product Details</h1>
-        <div className="bg-white p-4 rounded-lg shadow-md">
-          <img src={selectedProduct.imageUrl} alt={selectedProduct.name} className="w-full h-40 object-cover rounded" />
-          <h2 className="text-xl font-semibold mt-2">{selectedProduct.name}</h2>
-          <p className="text-gray-600">{selectedProduct.description}</p>
-          <p className="text-lg font-semibold mt-2">Price: ${selectedProduct.price}</p>
-          <button
-            onClick={() => handleAddToCart(selectedProduct)}
-            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 mt-2 rounded"
-          >
-            Add to Cart
-          </button>
+        <div>
+            <h1 className="text-3xl font-bold mb-4">Product not found</h1>
+            <p>This product does not exist or has been removed.</p>
         </div>
-      </div>
     );
-  }
-
-  // If the product doesn't exist (invalid ID), display a message or redirect to an error page
-  return (
-    <div>
-      <h1 className="text-3xl font-bold mb-4">Product not found</h1>
-      <p>This product does not exist or has been removed.</p>
-    </div>
-  );
 }
 
 export default ProductDetailPage;
