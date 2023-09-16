239d7b9aac70063a7762dddefd8371bb7409cd3e
VinaySohaliya <Sohaliyavin01@gmail.com>
2023-09-16 14:41:19 +0530

    sort by price and category funtionality added

---
+++ src/pages/components/sortByPrice.js
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import ProductCard from './productCard';
+import Link from 'next/link';
+
+function ProductList({ products }) {
+    const [sortedProducts, setSortedProducts] = useState(products);
+    const [categery, setcategery] = useState('');
+
+    const handleSortChange = (e) => {
+        const sortType = e.target.value;
+        let newSortedProducts = [...products];
+
+        if (sortType === 'asc') {
+            newSortedProducts.sort((a, b) => a.price - b.price);
+        } else if (sortType === 'desc') {
+            newSortedProducts.sort((a, b) => b.price - a.price);
+        }
+
+        setSortedProducts(newSortedProducts);
+    };
+     
+    function handleCategoty(e) {
+        const categery=e.target.value;
+        setcategery(categery);
+
+        if (categery=='All') {
+            setSortedProducts(products)
+        }else{
+            const categeryProducts=products.filter((p)=>p.category==categery)
+            setSortedProducts(categeryProducts)
+        }
+    }
+
+    return (
+        <div>
+            <label htmlFor="sort">Sort by Price:</label>
+            <select id="sort" onChange={handleSortChange}>
+                <option value='none'>By Default</option>
+                <option value="asc">Low to High</option>
+                <option value="desc">High to Low</option>
+            </select>
+
+            <label htmlFor="sort">Select Cetegory</label>
+            <select id="sort" onChange={handleCategoty}>
+                <option value='All'>All</option>
+                <option value="Clothing">Clothing</option>
+                <option value="Electronics">Electronics</option>
+            </select>
+
+
+
+            <div className=' flex'>
+                <div className="flex justify-evenly my-10 gap-4 ">
+                    {sortedProducts.map((product) => (
+                        <div key={product.id} className="bg-white p-4 rounded-lg shadow-md">
+                            <Link href={`/product/${product.id}`}>
+                                <ProductCard product={product} />
+                            </Link>
+                            <Link href={`/product/${product.id}`}>
+                                <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 mt-2 rounded">
+                                    View Product
+                                </button>
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default ProductList;
--- src/pages/men.js
+++ src/pages/men.js
@@ -1,65 +1,16 @@
 import React, { useState } from 'react';
 import products from './product.json';
-import ProductCard from './components/productCard';
-import Link from 'next/link';
+// import ProductCard from './components/productCard';
+// import Link from 'next/link';
+import ProductList from './components/sortByPrice';
 
 const MenPage = () => {
-  const [priceRange, setPriceRange] = useState({ min: 0, max: 1000 });
-  const [Pricevalue, setPricevalue] = useState({ min: '', max: '' });
 
-  const handlePriceChange = (e) => {
-    setPriceRange({ ...priceRange, [e.target.name]: e.target.value });
-  };
+
 
   return (
     <div className=' flex'>
-
-      <div>
-        <div className="mt-4">
-          <label htmlFor="minPrice" className="block text-sm font-medium text-gray-700">
-            Min Price
-          </label>
-          <input
-            type="range"
-            id="minPrice"
-            name="min"
-            value={priceRange.min}
-            onChange={handlePriceChange}
-            className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-          <p>{priceRange.min}</p>
-        </div>
-        <div className="mt-4">
-          <label htmlFor="maxPrice" className="block text-sm font-medium text-gray-700">
-            Max Price
-          </label>
-          <input
-            type="number"
-            id="maxPrice"
-            name="max"
-            value={priceRange.max}
-            onChange={handlePriceChange}
-            className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
-
-
-      </div>
-
-      <div className="flex justify-evenly my-10 gap-4 ">
-        {products.map((product) => (
-          <div key={product.id} className="bg-white p-4 rounded-lg shadow-md">
-            <Link href={`/product/${product.id}`}>
-              <ProductCard product={product} />
-            </Link>
-            <Link href={`/product/${product.id}`}>
-              <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 mt-2 rounded">
-                View Product
-              </button>
-            </Link>
-          </div>
-        ))}
-      </div>
+      <ProductList products={products}/>
     </div>
   );
 };
